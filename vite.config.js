18a4333f72b219303e3b244001e48eac20638881
srijanibanerje <srijaniofficial2022@gmail.com>
2026-05-08 12:51:04 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   preview: {
+      port: 5000,
+      allowedHosts: ['academysubadmin.onrender.com'], // ✅ add your Render domain here
+    },
 })
+ 
